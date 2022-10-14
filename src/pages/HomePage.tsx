5e5381a433715ad1d75b96c0a133aaf76fecd6af
Al Ros <alros.webdev@gmail.com>
2022-10-14 23:22:10 +0300

    Create logic for searching and saving/deleting to/from favorites

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,26 @@
-import React, { useState } from 'react'
-import { useSearchUsersQuery } from '../store/github/github.api'
+import React, { useEffect, useState } from 'react'
+import { RepoCard } from '../components/RepoCard'
+import { useDebounce } from '../hooks/debounce'
+import { useLazyGetUserReposQuery, useSearchUsersQuery } from '../store/github/github.api'
 
 export function HomePage() {
   const [search, setSearch] = useState('')
-  const {isLoading, isError, data} = useSearchUsersQuery('sasha')
+  const [dropdown, setDropdown] = useState(false)
+  const debounced = useDebounce(search)
+  const {isLoading, isError, data: users} = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 3,
+    refetchOnFocus: true
+  })
+  const [fetchRepos, { isLoading: areReposLoading, data: repos }] = useLazyGetUserReposQuery()
+
+  useEffect(() => {
+    setDropdown(debounced.length > 3 && users?.length! > 0)
+  }, [debounced, users])
+
+  const clickHandler = (username: string) => {
+    fetchRepos(username)
+    setDropdown(false)
+  }
 
   return (
     <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
@@ -17,8 +34,19 @@ export function HomePage() {
           value={search}
           onChange={e => setSearch(e.target.value)}
         />
-        <div className='absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto, laborum. 
+        {dropdown && <ul className='list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white'>
+          { isLoading && <p className='text-center'>Loading...</p> }
+          { users?.map(user => (
+            <li 
+              key={user.id}
+              onClick={() => clickHandler(user.login)}
+              className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
+              >{user.login}</li>
+          )) }
+        </ul>}
+        <div className="container">
+          { areReposLoading && <p className='text-center'>Repos are loading...</p> }
+          { repos?.map(repo => <RepoCard repo={repo} key={repo.id} />) }
         </div>
       </div>
     </div>
